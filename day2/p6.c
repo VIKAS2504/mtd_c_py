784818290705199fd32fef88e6eaca7e917021e9
VIKAS2504 <bmvikas25@gmail.com>
2025-03-07 13:14:43 +0530

    added

---
+++ day2/p6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int inputnum=0,i=0,ans=0;
+   puts("Enter the input number:");
+   scanf("%d",&inputnum);
+   for(i=0;i<=10;i++)
+   {
+    ans = inputnum * i;
+    printf("%d X %2d = %2d\n",inputnum,i,ans);
+
+   }
+   return 0;
+}
+    
