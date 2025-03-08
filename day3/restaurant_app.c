1996f368fd4f10c21d62ea0fd401faa52249775b
VIKAS2504 <bmvikas25@gmail.com>
2025-03-08 10:45:45 +0530

    added

--- day3/restaurant_app.c
+++ day3/restaurant_app.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     int foodType = 0, itemNumber = 0;
@@ -14,12 +15,11 @@ int main()
         switch (itemNumber)
         {
         case 1: puts("Yummy Davanagere Benne Dosa Maam"); break;
-        case 2: puts("Spicy khara bath and tasty Shira"); break;
+        case 2: puts("Spicy kahara bath and tasty Shira"); break;
         case 3: puts("Soft Idli and hot Vada Maam"); break;
         case 4: puts("Fresh Hot bath with Pure Ghee Sir"); break;
         default:puts("Sorry we dont serve your choice of food");
-        break;
-        }
+        } break;
     case 2: puts("1:Fish-Fry 2:Chicken-Biryani 3:Hanumantu-Palav 4:Egg-Masala. Your choice please: ");
         scanf("%d", &itemNumber);
         switch (itemNumber)
@@ -32,10 +32,8 @@ int main()
         } break;
         default:puts("This is Restaurtant, not Garden. Please Order an item");
     }
-    puts("Do you wish to have more? Yes:1 ,No:2");
-    scanf("&d",&itemNumber);
-
-    }while(itemNumber==1);
+    puts("Do you wish to have more? Yes:1, No:Any number");
+    scanf("%d", &itemNumber);
+    }while(itemNumber == 1);
     puts("Namaste Visit Again");
-    return 0;
 }
