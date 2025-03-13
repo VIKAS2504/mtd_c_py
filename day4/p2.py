918643bad5240de38e2d610f1f44e70309f41472
VIKAS2504 <bmvikas25@gmail.com>
2025-03-13 11:42:44 +0530

    added

--- day4/p2.py
+++ day4/p2.py
@@ -3,5 +3,5 @@ import math
 print("Enter a number to find its root")
 input_number = int(input()) # input function always returns string
 
-root_number = math.sqrt(input_number)
-print('Square root of', input_number, 'is'  + str(root_number)) 
+root_number = input_number ** 2
+print(f'Square root of',{input_number} is {root_number}'') 
